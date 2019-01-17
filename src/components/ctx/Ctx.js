30f7915e25849c5901fbc6a045c2920aa701b190
lixiaohui <13554223093@163.com>
2019-01-17 14:48:30 +0800

    132

--- src/components/ctx/Ctx.js
+++ src/components/ctx/Ctx.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Context from '../../context';
-const Ctx = () => {
-  return(
+
+const Ctx = () => (
     <div>
       <Context.Consumer>
         {theme => {
           return (
-            <div style={{width: '100%', height: '100px', backgroundColor: theme.bgColor, color: theme.color}}>{theme.txt}</div>
+            <div style={{
+              width: '100%',
+              height: '100px',
+              backgroundColor: theme.bgColor,
+              color: theme.color
+            }}>{theme.txt}</div>
           )
         }}
       </Context.Consumer>
       <p>提示：Context的参数一定要在Consumer里直接接收</p>
     </div>
   )
-};
+;
 export default Ctx;
